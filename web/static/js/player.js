9f143428ff0b063c7e4b83405c3a546ce17845b8
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-11-23 19:05:53 +0200

    Allow users to watch videos

---
+++ web/static/js/player.js
@@ -0,0 +1,35 @@
+let Player = {
+  player: null,
+
+  init(domId, playerId) {
+    window.onYouTubeIframeAPIReady = () => this.onIframeReady(domId, playerId)
+    let youTubeScriptTag = document.createElement("script")
+    youTubeScriptTag.src = "//www.youtube.com/iframe_api"
+    document.head.appendChild(youTubeScriptTag)
+  },
+
+  onIframeReady(domId, playerId) {
+    this.player = new YT.Player(domId, {
+      height: "360",
+      width: "420",
+      videoId: playerId,
+      events: {
+        "onReady": (event => this.onPlayerReady(event)),
+        "onStateChange": (event => this.onPlayerStateChange(event))
+      }
+    })
+  },
+
+  onPlayerStateChange(event) { },
+
+  onPlayerReady(event) { },
+
+  getCurrentTime() {
+    return Math.floor(this.player.getCurrentTime() * 1000)
+  },
+
+  seekTo(millsec) {
+    return this.player.seekTo(millsec/ 1000)
+  }
+}
+export default Player
